85b42110d54145b130ede2de0016fd1f0a4288bc
cbrzn <cesarbrazon10@gmail.com>
2017-02-19 17:12:48 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ludus box"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title" , "#{@base_title} | Home"
   end
 
   test "should get help" do
