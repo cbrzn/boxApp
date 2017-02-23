f15072b1f90ce9d95c722edc4568f31b187f325c
cbrzn <cesarbrazon10@gmail.com>
2017-02-22 23:23:19 -0400

    Accout activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
    host = '<your heroku app>.herokuapp.com'
-   config.action_mailer.default_url_options = { host: boxreserva }
+   config.action_mailer.default_url_options = { host: https://boxreserva }
    ActionMailer::Base.smtp_settings = {
      :address        => 'smtp.sendgrid.net',
      :port           => '587',
