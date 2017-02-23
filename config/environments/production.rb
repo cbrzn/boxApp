0c9d9833ba3280804a9dd3d026a9c2890788c1ae
cbrzn <cesarbrazon10@gmail.com>
2017-02-22 23:47:12 -0400

    Finally done autentication user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
-   host = '<your heroku app>.herokuapp.com'
-   config.action_mailer.default_url_options = { host: https://boxreserva.herokuapp.com }
+   host = 'https://boxreserva.herokuapp.com'
+   config.action_mailer.default_url_options = { host: host }
    ActionMailer::Base.smtp_settings = {
      :address        => 'smtp.sendgrid.net',
      :port           => '587',
