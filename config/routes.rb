890cf665e4ab41c9a4f34849bbb88c42702fc163
cbrzn <cesarbrazon10@gmail.com>
2017-02-23 22:35:17 -0400

    Showing training hours

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get    '/signup',    to: 'users#new'
   get    '/contact',   to: 'static_pages#contact'
   get    '/about',     to: 'static_pages#about'
+
   get    '/login',     to: 'sessions#new'
   post   '/login',     to: 'sessions#create'
   delete '/logout',    to: 'sessions#destroy'
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
