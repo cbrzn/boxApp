e6b2ce8393e934bd0bddbcd6de20af0532256324
cbrzn <cesarbrazon10@gmail.com>
2017-03-02 15:47:49 -0400

    Changing seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,31 +4,19 @@ User.create!(name: "Cesar",
             password: 'foobar',
             password_confirmation: 'foobar')
 
-Training.create!(hora: "7:00am",
-                 cantidad: 24)
-Training.create!(hora: "8:00am",
-                 cantidad: 24)
-Training.create!(hora: "9:00am",
-                 cantidad: 24)
-Training.create!(hora: "10:00am",
-                 cantidad: 24)
-Training.create!(hora: "11:00am",
-                 cantidad: 24)
-Training.create!(hora: "12:00m",
-                 cantidad: 24)
-Training.create!(hora: "1:00pm",
-                 cantidad: 24)
-Training.create!(hora: "2:00pm",
-                 cantidad: 24)
-Training.create!(hora: "3:00pm",
-                 cantidad: 24)
-Training.create!(hora: "4:00pm",
-                 cantidad: 24)
-Training.create!(hora: "5:00pm",
-                 cantidad: 24)
-Training.create!(hora: "6:00pm",
-                 cantidad: 24)
-Training.create!(hora: "7:00pm",
-                 cantidad: 24)
-Training.create!(hora: "8:00pm",
-                 cantidad: 24)
+training_hours =  [
+                  [ "7:00am - 8:00am",   24],
+                  [ "8:00am - 9:00am",   24],
+                  [ "9:00am - 10:00am",  24],
+                  [ "10:00am - 11:00am", 24],
+                  [ "11:00am - 12:00am", 24],
+                  [ "12:00am - 1:00am",  24],
+                  [ "1:00am - 2:00am",   24],
+                  [ "2:00am - 3:00am",   24],
+                  [ "3:00am - 4:00am",   24],
+                  [ "4:00am - 5:00am",   24],
+                  ]
+
+training_hours.each do |hora, cantidad|
+Training.create!( hora: hora, cantidad: cantidad)
+end
