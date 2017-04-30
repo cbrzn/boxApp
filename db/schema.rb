039b2bb9a4ca9fc3a2d751ecc4dd61ff06881f3b
Cesar Brazon <cesarbrazon10@gmail.com>
2017-04-30 15:16:57 -0400

    A

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430191013) do
+ActiveRecord::Schema.define(version: 20170430191645) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170430191013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "max"
+    t.integer  "quantity"
     t.index ["user_id", "created_at"], name: "index_rms_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_rms_on_user_id"
   end
