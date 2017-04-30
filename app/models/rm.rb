e1153fc4eafedcaefa3f660b5e29e197be3cae7a
Cesar Brazon <cesarbrazon10@gmail.com>
2017-04-30 05:38:55 -0400

    Fixing bug

--- app/models/rm.rb
+++ app/models/rm.rb
@@ -1,6 +1,7 @@
 class Rm < ApplicationRecord
-  belongs_to :user, inverse_of: :rms
+  belongs_to :user
   scope :content, -> { where(featured: true) }
+  validates_presence_of :quantity, :content
 
   def self.latest_rm
     order('created_at desc').first
